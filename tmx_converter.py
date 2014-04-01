e5fa14b2254bb1565c44bf07a2c4c2c8bb05b4fd
Ben Longbons <b.r.longbons@gmail.com>
2014-03-31 17:57:06 -0700

    Silence new warnings in generated mob scripts

--- tmx_converter.py
+++ tmx_converter.py
@@ -301,10 +301,10 @@ class ContentHandler(xml.sax.ContentHandler):
                 self.state = State.FINAL
 
     def endDocument(self):
-        self.mobs.write('\n\n%s.gat,0,0,0|script|Mob%s|-1,\n{\n' % (self.base, self.base))
+        self.mobs.write('\n\n%s.gat,0,0,0|script|Mob%s|-1,\n{\n    end;\n' % (self.base, self.base))
         for mob_id in sorted(self.mob_ids):
-            self.mobs.write('On%d:\n    set @mobID, %d;\n    callfunc "MobPoints";\n    end;\n\n' % (mob_id, mob_id))
-        self.mobs.write('    end;\n}\n')
+            self.mobs.write('\nOn%d:\n    set @mobID, %d;\n    callfunc "MobPoints";\n    end;\n' % (mob_id, mob_id))
+        self.mobs.write('}\n')
         self.imports.write('// Map %s: %s\n' % (self.base, self.name))
         self.imports.write('// %s\n' % MESSAGE)
         self.imports.write('map: %s.gat\n' % self.base)
