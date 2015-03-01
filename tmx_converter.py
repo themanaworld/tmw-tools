a49ce96049a70e35336e93814282287039b9fa31
wushin <pasekei@gmail.com>
2015-02-28 19:39:58 -0600

    Make Maps rebuild less

--- tmx_converter.py
+++ tmx_converter.py
@@ -133,6 +133,7 @@ class ContentHandler(xml.sax.ContentHandler):
         self.imports = imports
         self.object = None
         self.mob_ids = set()
+        self.name_unique = 0;
 
     def setDocumentLocator(self, loc):
         self.locator = loc
@@ -265,10 +266,10 @@ class ContentHandler(xml.sax.ContentHandler):
                     ])
                 )
             elif isinstance(obj, Warp):
-                obj_name = obj.name[:19]
+                obj_name = obj.name[:15]
                 if obj.name != obj_name:
                     print('Warning: warp name truncated: %r -> %r' % (obj.name, obj_name))
-                obj_name += "#" + str(main.name_unique)
+                obj_name += "#%s%d" % (self.base,self.name_unique)
                 self.warps.write(
                     SEPARATOR.join([
                         '%s,%d,%d' % (self.base, obj.x, obj.y),
@@ -277,7 +278,7 @@ class ContentHandler(xml.sax.ContentHandler):
                         '%d,%d,%s,%d,%d\n' % (obj.w, obj.h, obj.dest_map, obj.dest_tile_x, obj.dest_tile_y),
                     ])
                 )
-                main.name_unique += 1
+                self.name_unique += 1
 
         if name == u'data':
             if self.state is State.DATA:
@@ -319,7 +320,6 @@ def main(argv):
     tmx_dir = posixpath.join(client_data, CLIENT_MAPS)
     wlk_dir = posixpath.join(server_data, SERVER_WLK)
     npc_dir = posixpath.join(server_data, SERVER_NPCS)
-    main.name_unique = 0;
     if check_mobs:
         global mob_names
         mob_names = {}
