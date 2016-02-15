5fb8ed25c2f77259e3ac17462a553a54508883ae
Andrei Karas <akaras@inbox.ru>
2016-02-15 22:37:20 +0300

    testxml: add xsdcheck script for check xml files based on schemes.

---
+++ testxml/xsd/checkfile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+xmllint --format --schema tmw.xsd "${1}" 2>&1 >/dev/null | \
+    grep -v ": Skipping import of schema located at " | \
+    grep -v ".xml validates"
---
+++ testxml/xsdcheck.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cd xsd
+DIR="$(pwd)/../../../client-data"
+rm ../errors.txt
+
+function check {
+    xmllint --format --schema tmw.xsd "${DIR}"/"${1}" 2>&1 >/dev/null | \
+        grep -v ": Skipping import of schema located at " | \
+        grep -v ".xml validates" | \
+        grep -v ".manaplus validates" >>../errors.txt
+}
+
+xmllint --format --schema XMLSchema.xsd tmw.xsd 2>&1 >/dev/null | \
+    grep -v ": Skipping import of schema located at " | \
+    grep -v ".xsd validates" >>../errors.txt
+
+check avatars.xml
+check charcreation.xml
+check deadmessages.xml
+check effects.xml
+check emotes.xml
+check equipmentwindow.xml
+check evol.manaplus
+check features.xml
+check itemcolors.xml
+check items.xml
+check maps.xml
+check mods.xml
+check monsters.xml
+check npcdialogs.xml
+check npcs.xml
+check paths.xml
+check pets.xml
+check quests.xml
+check skills.xml
+check skillunits.xml
+check sounds.xml
+check status-effects.xml
+check units.xml
+check weapons.xml
+
+find "${DIR}/graphics" -type f -name "*.xml" -exec ./checkfile.sh {} \; >>../errors.txt
