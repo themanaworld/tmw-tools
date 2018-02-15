9d0f7196eaa963644beb94a83eeefba0d1776c92
jesusalva <cpntb1@ymail.com>
2018-02-15 12:04:09 -0200

    Force LICENSECHECK to check maps and JPG images

--- licensecheck/clientdata.sh
+++ licensecheck/clientdata.sh
@@ -4,3 +4,6 @@ export DIR="../../client-data"
 
 find -H $DIR -type f -name "*.png" -exec ./checkfile.sh $DIR/LICENSE {} \;
 find -H $DIR/sfx -type f -name "*.ogg" -exec ./checkfile.sh $DIR/LICENSE {} \;
+find -H $DIR -type f -name "*.tmx" -exec ./checkfile.sh $DIR/LICENSE {} \;
+find -H $DIR -type f -name "*.jpg" -exec ./checkfile.sh $DIR/LICENSE {} \;
+
