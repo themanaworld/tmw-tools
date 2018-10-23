4929ba85b53e3c018acdbab5b9f4f66fdd569d38
Jesusaves <cpntb1@ymail.com>
2018-10-23 09:23:12 +0000

    Like a ninja

--- licensecheck/clientdata.sh
+++ licensecheck/clientdata.sh
@@ -2,8 +2,8 @@
 
 export DIR="../../client-data"
 
-find -H $DIR -type f -name "*.png" -exec ./checkfile.sh $DIR/LICENSE $DIR/SAULC.txt {} \;
-find -H $DIR/sfx -type f -name "*.ogg" -exec ./checkfile.sh $DIR/LICENSE $DIR/SAULC.txt {} \;
-find -H $DIR -type f -name "*.tmx" -exec ./checkfile.sh $DIR/LICENSE $DIR/SAULC.txt {} \;
-find -H $DIR -type f -name "*.jpg" -exec ./checkfile.sh $DIR/LICENSE $DIR/SAULC.txt {} \;
+find -H $DIR -type f -name "*.png" -exec ./checkfile.sh $DIR/LICENSE $DIR ART_LICENSE {} \;
+find -H $DIR/sfx -type f -name "*.ogg" -exec ./checkfile.sh $DIR/LICENSE $DIR/ART_LICENSE {} \;
+find -H $DIR -type f -name "*.tmx" -exec ./checkfile.sh $DIR/LICENSE $DIR/ART_LICENSE {} \;
+find -H $DIR -type f -name "*.jpg" -exec ./checkfile.sh $DIR/LICENSE $DIR/ART_LICENSE {} \;
 
